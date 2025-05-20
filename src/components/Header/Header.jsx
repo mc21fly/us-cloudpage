ac2c64260ac882c28e2f7a9f5f2129dc99bc3a19
Michal Blakala <michal.blakala@performanceart.com>
2025-05-20 18:25:11 +0200

    feedback

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -26,8 +26,8 @@ export default function Header() {
             <div className="container burg">
                 <div className="container hero pad">
                     <h1>
-                        For over 155 years, we've been helping <br className="mbl-hide" />
-                        clients reach their unique goals
+                        For over 155 years, we've been helping <br className="mbl-hide hide-xl" />
+                        clients reach their unique&nbsp;goals
                     </h1>
                 </div>
             </div>
